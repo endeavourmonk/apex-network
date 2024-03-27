de764b432f2bf1427aaaa46f814d4b4dd56878c9
Ujjawal Kumar <ujjawal.kumar.0831@gmail.com>
2024-03-27 23:47:54 +0530

    created schemas for resources(users, posts, jobs, connections, applications)

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
-import express from 'express'
+import 'dotenv/config';
+import express from 'express';
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, world!')
-})
+  res.send('Hello, world!');
+});
+
+console.log(process.env.PORT);
 
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`)
-})
+  console.log(`🖥  Server is running at http://localhost:${port}⛁`);
+});
