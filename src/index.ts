756fa6779e25e80aa8bda0c2d8f6cd110d4dffd4
Ujjawal Kumar <ujjawal.kumar.0831@gmail.com>
2024-03-29 00:21:22 +0530

    in test- created service repos

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,53 @@
 import 'dotenv/config';
+import 'reflect-metadata';
 import express from 'express';
 
-const app = express();
-const port = 3000;
+import { container, inject } from 'tsyringe';
+import { UserRepository } from './repositories/userRepository.interface';
+import { UserRepositoryPrisma } from './repositories/userRepositoryPrisma.ts';
+import { User } from '@prisma/client';
+container.register<UserRepository>('UserRepository', {
+  useClass: UserRepositoryPrisma,
+});
 
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(express.json({ limit: '10kb' }));
+
+export interface IUserService {
+  create(user: any): Promise<User>;
+}
+
+class UserService implements IUserService {
+  constructor(
+    @inject('UserRepository') private userRepository: UserRepository,
+  ) {}
+
+  async create(user: any) {
+    return this.userRepository.create(user);
+  }
+}
+
+container.register<IUserService>('IUserService', {
+  useClass: UserService,
+});
+
+app.post('/', async (req, res) => {
+  try {
+    const val: IUserService = container.resolve('IUserService');
+    console.log(val);
+    // await val.create(req.body);
+    res.status(200).json({
+      status: 'success',
+    });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 console.log(process.env.PORT);
 
-app.listen(port, () => {
-  console.log(`🖥  Server is running at http://localhost:${port}⛁`);
+app.listen(PORT, () => {
+  console.log(`🖥  Server is running at http://localhost:${PORT}⛁`);
 });
