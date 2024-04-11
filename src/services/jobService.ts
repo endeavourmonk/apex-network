dcf25a0162f31d3f161845500852e7109519fac5
Ujjawal Kumar <ujjawal.kumar.0831@gmail.com>
2024-04-11 21:22:24 +0530

    created job service interface and service and job controller

---
+++ src/services/jobService.ts
@@ -0,0 +1,25 @@
+import { inject, injectable } from 'tsyringe';
+import { JobRepository } from '../repositories/jobRepository.interface';
+import { IJobService } from './jobService.interface';
+import { Job } from '@prisma/client';
+
+@injectable()
+export class JobService implements IJobService {
+  constructor(@inject('JobRepository') private JobRepository: JobRepository) {}
+
+  getAll(): Promise<Job[]> {
+    return this.JobRepository.getAll();
+  }
+  getById(JobID: number): Promise<Job | null> {
+    return this.JobRepository.getById(JobID);
+  }
+  create(Job: Omit<Job, 'JobID'>): Promise<Job> {
+    return this.JobRepository.create(Job);
+  }
+  update(JobID: number, Job: Job): Promise<Job | null> {
+    return this.JobRepository.update(JobID, Job);
+  }
+  delete(JobID: number): Promise<boolean> {
+    return this.JobRepository.delete(JobID);
+  }
+}
